46d442ec77c28f9aa433001faf4bb03930f04d43
Mason Austin <mason47austin@gmail.com>
2023-07-13 18:59:13 -0500

    created handle click for the buttons

--- components/Square.js
+++ components/Square.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Square({ value }) {
-  return <button type="button" className="square">{value}</button>;
+  const handleClick = () => { console.warn(value); };
+  return <button type="button" onClick={handleClick} className="square">{value}</button>;
 }
 
 export default Square;
 
-Square.propTypes = { value: PropTypes.string.isRequired };
+Square.propTypes = { value: PropTypes.number.isRequired };
