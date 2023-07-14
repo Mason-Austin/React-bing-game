0ef4fd6d8ca66f8026d034592d2aa0df58bccf4e
Mason Austin <mason47austin@gmail.com>
2023-07-13 19:08:06 -0500

    made handle click change empty spaces to x

--- components/Square.js
+++ components/Square.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
 
-function Square({ value }) {
-  const handleClick = () => { console.warn(value); };
+function Square() {
+  const [value, setValue] = useState(null);
+  const handleClick = () => { setValue('x'); };
   return <button type="button" onClick={handleClick} className="square">{value}</button>;
 }
 
 export default Square;
-
-Square.propTypes = { value: PropTypes.number.isRequired };
