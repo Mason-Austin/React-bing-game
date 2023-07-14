1fb4a881014e53c10f1d79f4e9fd1b2b796c2ede
Mason Austin <mason47austin@gmail.com>
2023-07-13 19:34:50 -0500

    created handleClick function that loops through an array to change the value of the square you clicked

--- components/Square.js
+++ components/Square.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-function Square() {
-  const [value, setValue] = useState(null);
-  const handleClick = () => { setValue('x'); };
-  return <button type="button" onClick={handleClick} className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  return <button type="button" onClick={onSquareClick} className="square">{value}</button>;
 }
 
 export default Square;
+
+Square.propTypes = {
+  value: PropTypes.string.isRequired,
+  onSquareClick: PropTypes.func.isRequired,
+};
